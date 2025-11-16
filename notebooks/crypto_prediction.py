6a1456e52fd41359fc1154e0fad5a4850f7633ed
Sahar Sistani <saharsistani137777@gmail.com>
2025-11-16 11:42:25 +0330

    Add sample python file for crypto project

---
+++ notebooks/crypto_prediction.py
@@ -0,0 +1,18 @@
+# Sample Crypto Price Prediction
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Sample data
+data = pd.DataFrame({
+    "Date": ["2025-11-10", "2025-11-11", "2025-11-12"],
+    "Close": [50000, 51000, 49500]
+})
+
+print(data)
+
+plt.plot(data['Date'], data['Close'])
+plt.title('Bitcoin Closing Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
