07243c7f4f73dde1992cd414faa08675befb80a9
Sahar Sistani <saharsistani137777@gmail.com>
2025-11-16 11:52:20 +0330

    Add plot saving code for crypto project

--- notebooks/crypto_prediction.py
+++ notebooks/crypto_prediction.py
@@ -16,3 +16,23 @@ plt.title('Bitcoin Closing Price')
 plt.xlabel('Date')
 plt.ylabel('Price')
 plt.show()
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Sample data
+data = pd.DataFrame({
+    "Date": ["2025-11-10", "2025-11-11", "2025-11-12"],
+    "Close": [50000, 51000, 49500]
+})
+
+# Plot closing price
+plt.plot(data['Date'], data['Close'])
+plt.title('Bitcoin Closing Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+
+# Save the plot into images folder
+plt.savefig('crypto_price_prediction/images/price_plot.png')
+plt.show()
+
