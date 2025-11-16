74d1f7c8162224c6010c87610f68376b97b1d472
Sahar Sistani <saharsistani137777@gmail.com>
2025-11-16 12:16:36 +0330

    Fix crypto prediction code and save plot correctly

--- notebooks/crypto_prediction.py
+++ notebooks/crypto_prediction.py
@@ -2,6 +2,11 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+# make imgs fldr
+if not os.path.exists('crypto_price_prediction/images'):
+    os.makedirs('crypto_price_prediction/images')
 
 # Sample data
 data = pd.DataFrame({
@@ -11,28 +16,13 @@ data = pd.DataFrame({
 
 print(data)
 
-plt.plot(data['Date'], data['Close'])
-plt.title('Bitcoin Closing Price')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.show()
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Sample data
-data = pd.DataFrame({
-    "Date": ["2025-11-10", "2025-11-11", "2025-11-12"],
-    "Close": [50000, 51000, 49500]
-})
-
-# Plot closing price
+# graph
 plt.plot(data['Date'], data['Close'])
 plt.title('Bitcoin Closing Price')
 plt.xlabel('Date')
 plt.ylabel('Price')
 
-# Save the plot into images folder
+# img
 plt.savefig('crypto_price_prediction/images/price_plot.png')
 plt.show()
 
